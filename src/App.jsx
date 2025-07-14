18f9f5d381959b1ac281cbd1eb45b9167345df03
Emad Abdelmaksoud <eabdelmaksoud@proton.me>
2025-07-13 20:06:28 -0700

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
-import AuthForm from "./form/AuthForm";
-import * as yup from "yup";
+import AuthForm from '~/AuthForm';
+import * as yup from 'yup';
 
 function App() {
-  const onLogin = (values) => {
-    // dispatch form values to an API here
-    alert("Success!");
-  };
-  const onRegister = (values) => {
-    // dispatch form values to an API here
-    alert("Success!");
-  };
-  return (
-    <AuthForm
-      onSignIn={onLogin}
-      onSignUp={onRegister}
-      validationSchema={yup.object({
-        username: yup
-          .string()
-          .max(15, "Must be 15 characters or less")
-          .required("Please provide a username"),
-        email: yup
-          .string()
-          .email("Invalid email address")
-          .required("Please provide an email"),
-        password: yup
-          .string()
-          .max(9, "Must be 9 characters or less")
-          .required("Please provide a password"),
-      })}
-      initalValues={{
-        username: "",
-        email: "",
-        password: "",
-      }}
-    />
-  );
+	const onLogin = (values) => {
+		// dispatch form values to an API here
+		alert('Success!');
+	};
+	const onRegister = (values) => {
+		// dispatch form values to an API here
+		alert('Success!');
+	};
+	return (
+		<AuthForm
+			onSignIn={onLogin}
+			onSignUp={onRegister}
+			validationSchema={yup.object({
+				username: yup
+					.string()
+					.max(15, 'Must be 15 characters or less')
+					.required('Please provide a username'),
+				email: yup
+					.string()
+					.email('Invalid email address')
+					.required('Please provide an email'),
+				password: yup
+					.string()
+					.max(9, 'Must be 9 characters or less')
+					.required('Please provide a password'),
+			})}
+			initalValues={{
+				username: '',
+				email: '',
+				password: '',
+			}}
+		/>
+	);
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        libInjectCss()
-    ],
-    build: {
-        lib: {
-            // src/indext.js is where we have exported the component(s)
-            entry: resolve(__dirname, "src/lib/index.js"),
-            name: "auth-form-react",
-            // the name of the output files when the build is run
-            fileName: "auth-form-react",
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ["react", "react-dom", "react-router-dom", "formik", "yup"],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    react: "React",
-                    "react-dom": "React-dom",
-                    "formik": "formik",
-                    "yup": "yup"
-
-                },
-            }
-        },
-    },
-})
+	plugins: [react(), libInjectCss()],
+	resolve: {
+		alias: {
+			'~': fileURLToPath(new URL('./src/form', import.meta.url)), // Alias for src folder
+		},
+	},
+	build: {
+		lib: {
+			// src/indext.js is where we have exported the component(s)
+			entry: resolve(__dirname, 'src/lib/index.js'),
+			name: 'auth-form-react',
+			// the name of the output files when the build is run
+			fileName: 'auth-form-react',
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['react', 'react-dom', 'react-router-dom', 'formik', 'yup'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					react: 'React',
+					'react-dom': 'React-dom',
+					formik: 'formik',
+					yup: 'yup',
+				},
+			},
+		},
+	},
+});
